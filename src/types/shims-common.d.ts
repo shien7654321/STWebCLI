a0cdab5508d9b31698a3ad9bd4b144f008652d25
Suta <shien7654321@163.com>
2022-12-09 16:05:49 +0800

    Update dependencies.

--- src/types/shims-common.d.ts
+++ src/types/shims-common.d.ts
@@ -1,2 +1,6 @@
 declare module '*.module.css';
 declare module '*.module.less';
+declare module '*.module.sass';
+declare module '*.module.scss';
+declare module '*.module.styl';
+declare module '*.module.stylus';

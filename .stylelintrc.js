0245d974f38eef5ea7ff32568d3762a8b4af84b2
Suta <shien7654321@163.com>
2023-04-24 21:05:22 +0800

    1. Update dependencies; 2. Update .stylelintrc.js.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,25 @@
 module.exports = {
     extends: [
         'stylelint-config-standard',
+        'stylelint-config-recess-order',
+    ],
+    overrides: [
+        {
+            files: '**/*.less',
+            customSyntax: 'postcss-less',
+        },
+        {
+            files: '**/*.sass',
+            customSyntax: 'postcss-sass',
+        },
+        {
+            files: '**/*.scss',
+            customSyntax: 'postcss-scss',
+        },
+        {
+            files: ['**/*.styl', '**/*.stylus'],
+            customSyntax: 'postcss-styl',
+        },
     ],
     rules: {
         indentation: 4,

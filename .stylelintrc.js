b58fcdace11704ce475a37fa71bbac9435ec9df8
Suta <shien7654321@163.com>
2023-04-24 20:48:55 +0800

    1. Update dependencies; 2. Update .stylelintrc.js.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,6 +2,25 @@ module.exports = {
     extends: [
         'stylelint-config-standard',
         'stylelint-config-recommended-vue',
+        'stylelint-config-recess-order',
+    ],
+    overrides: [
+        {
+            files: '**/*.less',
+            customSyntax: 'postcss-less',
+        },
+        {
+            files: '**/*.sass',
+            customSyntax: 'postcss-sass',
+        },
+        {
+            files: '**/*.scss',
+            customSyntax: 'postcss-scss',
+        },
+        {
+            files: ['**/*.styl', '**/*.stylus'],
+            customSyntax: 'postcss-styl',
+        },
     ],
     rules: {
         indentation: 4,

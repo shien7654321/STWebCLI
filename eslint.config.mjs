b55a5c58acacffc86137d79d1b9b27398e8bd062
Suta <shien7654321@163.com>
2025-03-20 19:46:51 +0800

    Update eslint.config.mjs.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,7 +9,6 @@ import tsPlugin from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import reactPlugin from 'eslint-plugin-react';
 import prettierPluginRecommend from 'eslint-plugin-prettier/recommended';
-import { version } from 'node:os';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
